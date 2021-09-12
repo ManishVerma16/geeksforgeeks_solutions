8294db639ca4bca20b12110a48981981d52080a1
Manish Verma <vermam318@gmail.com>
2021-09-12 22:24:21 +0530

    equilibrium

---
+++ cpp/equilibriumPoint.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int equilibriumPoint(long long arr[], int n){
+	
+
+}
+
+int main()
+{
+	/* code */
+	long long n;
+	cin >> n;
+	long long a[n];
+	for (long long i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	cout << equilibriumPoint(a, n) << endl;
+	return 0;
+}
