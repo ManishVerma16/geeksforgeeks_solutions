f436b3529f3ab06eaa35248c1e66bb42eb0efb9c
ManishVerma16 <vermam318@gmail.com>
2022-03-20 23:40:00 +0530

    anagram

---
+++ java/Anagram.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Anagram {
+    public static void main(String[] args) {
+        String a = "geeksforgeeks";
+        String b = "forgeeksgeeks";
+        // boolean flag = true;
+        System.out.println(a.contains(b));
+        // String[] arr1 = a.split("");
+        // String[] arr2 = b.split("");
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        // if (a.length() != b.length()) {
+        //     // return false;
+        // }
+        // else{
+        //     for (int i = 0; i < arr1.length; i++) {
+        //         System.out.println(arr1[i] + " " + arr2[i]);
+        //         if (arr1[i].equals(arr2[i])) {
+        //             flag = true;
+        //         } else {
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        // }
+    //   return flag; 
+    }
+}
